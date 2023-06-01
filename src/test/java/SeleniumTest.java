b8292df041d70f25f31ac72545f19100f476a242
RoryEiffe <roryeiffe8@gmail.com>
2023-06-01 10:36:39 -0400

    Added tests

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumTest {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        // Set up ChromeDriver path
+        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+
+        // Create a new ChromeDriver instance
+        driver = new ChromeDriver();
+        File file = new File("ButtonClicker.html");
+        // Open the HTML file
+        driver.get(file.getAbsolutePath());
+    }
+    @Test
+    public void testButton1() {
+        // check the initial state of the text:
+        WebElement displayElement = driver.findElement(By.id("text1"));
+        assertEquals("click the button ...", displayElement.getText());
+
+        // Find button 1 and click it
+        WebElement button1 = driver.findElement(By.id("button1"));
+        button1.click();
+
+        assertEquals("button 1 clicked", displayElement.getText());
+    }
+
+    @Test
+    public void testButton2() {
+        // check the initial state of the text:
+        WebElement displayElement = driver.findElement(By.id("text2"));
+        assertEquals("now click this button ...", displayElement.getText());
+
+        // Find button 2 and click it
+        WebElement button2 = driver.findElement(By.id("button2"));
+        button2.click();
+
+        assertEquals("button 2 clicked", displayElement.getText());
+    }
+
+    @After
+    public void tearDown() {
+        // Close the browser
+        driver.quit();
+    }
+}
